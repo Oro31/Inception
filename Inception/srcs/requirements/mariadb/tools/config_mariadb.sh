1b3aef03408cf3489eba381e6f25da8512137270
Remi <remi_valton@laposte.net>
2022-12-05 15:56:52 +0100

    worpress installation is needed

--- Inception/srcs/requirements/mariadb/tools/config_mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/config_mariadb.sh
@@ -6,6 +6,6 @@ echo "CREATE USER IF NOT EXISTS 'rvalton'@'%' IDENTIFIED BY 'password';" | maria
 
 echo "GRANT ALL ON *.* TO 'rvalton'@'%' IDENTIFIED BY 'password'; FLUSH PRIVILEGES; " | mariadb -u root
 
-echo "CREATE DATABASE IF NOT EXISTS db; GRANT ALL ON db.* TO 'rvalton'@'%' IDENTIFIED BY 'password'; FLUSH PRIVILEGES;" | mariadb -u root
+echo "CREATE DATABASE IF NOT EXISTS mariadb; GRANT ALL ON mariadb.* TO 'rvalton'@'%' IDENTIFIED BY 'password'; FLUSH PRIVILEGES;" | mariadb -u root
 
 exec "$@"
