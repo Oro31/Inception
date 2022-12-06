f3448617f9c77208c43bdd50e319cd9b6835ee9a
Oro31 <rvalton@student.42.fr>
2022-12-06 17:10:58 +0100

    can be good but still some mysteries in mariadb

--- Inception/srcs/requirements/mariadb/tools/config_mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/config_mariadb.sh
@@ -2,11 +2,15 @@
 
 /etc/init.d/mysql start
 
-echo "CREATE USER IF NOT EXISTS 'rvalton'@'%' IDENTIFIED BY 'password';" | mariadb -u root
+echo "CREATE USER IF NOT EXISTS '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';" | mariadb -u root
 
-echo "GRANT ALL ON *.* TO 'rvalton'@'%' IDENTIFIED BY 'password'; FLUSH PRIVILEGES; " | mariadb -u root
+echo "GRANT ALL ON *.* TO '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}'; FLUSH PRIVILEGES; " | mariadb -u root
 
-echo "CREATE DATABASE IF NOT EXISTS mariadb; GRANT ALL ON mariadb.* TO 'rvalton'@'%' IDENTIFIED BY 'password'; FLUSH PRIVILEGES;" | mariadb -u root
+echo "CREATE DATABASE IF NOT EXISTS ${MARIA_DATABASE}; GRANT ALL ON ${MARIA_DATABASE}.* TO '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}'; FLUSH PRIVILEGES;" | mariadb -u root
+
+#mysql -u root ${MARIA_DATABASE} < exported.sql
+
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" | mariadb -u root
 
 /etc/init.d/mysql stop
 
