f4a210044234194c6f3584f2e03a737cad11ac1d
Oro31 <rvalton@student.42.fr>
2022-12-08 11:57:37 +0100

    mariadb not crashing anymore le flag --disable-unix-socket qui carry

--- Inception/srcs/requirements/mariadb/tools/config_mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/config_mariadb.sh
@@ -2,15 +2,24 @@
 
 /etc/init.d/mysql start
 
+mysql_secure_installation <<_EOF_
+
+y
+${MARIA_ROOT_PASSWORD}
+${MARIA_ROOT_PASSWORD}
+y
+y
+y
+y
+_EOF_
+ 
 echo "CREATE USER IF NOT EXISTS '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';" | mariadb -u root
 
 echo "GRANT ALL ON *.* TO '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}'; FLUSH PRIVILEGES; " | mariadb -u root
 
 echo "CREATE DATABASE IF NOT EXISTS ${MARIA_DATABASE}; GRANT ALL ON ${MARIA_DATABASE}.* TO '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}'; FLUSH PRIVILEGES;" | mariadb -u root
 
-#mysql -u root ${MARIA_DATABASE} < exported.sql
-
-#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" | mariadb -u root
+mysql -u ${MARIA_USER} "-ppassword" ${MARIA_DATABASE} < wp_exported.sql
 
 /etc/init.d/mysql stop
 
